da1a5532bde809e37ff2de8d8df45c161a4e8f7d
Greg Zoller <gzoller@gmail.com>
2012-05-24 16:35:50 -0500

    Changed sbt to 0.11.2 and scala to 2.9.1, which is used by sbt 0.11.2

--- build.sbt
+++ build.sbt
@@ -4,6 +4,6 @@ name := "sbt-mongo-plugin"
 
 organization := "co.nubilus"
 
-version := "0.1"
+version := "0.2"
 
 scalaVersion := "2.9.1"
--- src/main/scala/co/nubilus/SbtMongoPlugin.scala
+++ src/main/scala/co/nubilus/SbtMongoPlugin.scala
@@ -7,11 +7,11 @@ import Process._
 
 object SbtMongoPlugin extends Plugin
 {
-	val mongoTask = TaskKey[Unit]("mongo-task","Load fixture data into mongo")
+	val mongoTask = TaskKey[Unit]("mongo-load","Load fixture data into mongo")
 	val mongoFixtureDirectory = SettingKey[File]("mongo-fixture-directory","Directory where mongo fixtures will be found")
 	val mongoMigrationDirectory = SettingKey[File]("mongo-migration-directory","Directory where mongo migration scripts will be found")
 
-	lazy val mongoSettings:Seq[Project.Setting[_]] = Seq(
+	val mongoSettings  = Seq(
 		mongoFixtureDirectory <<= (unmanagedResourceDirectories in Test).apply({ (f) => new java.io.File(f.head, "/fixtures") }),
 		mongoMigrationDirectory <<= mongoFixtureDirectory,
 		mongoTask <<= (mongoFixtureDirectory, mongoMigrationDirectory) map { 
